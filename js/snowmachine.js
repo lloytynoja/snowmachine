6228adaea836191c43bb92b19af9354e9753e701
lloytynoja <loytynoja@gmail.com>
2013-03-10 10:37:45 +0200

    flake object added

--- js/snowmachine.js
+++ js/snowmachine.js
@@ -5,15 +5,32 @@ var ns = {};
 ns.main = function main() {
 	ns.canvas = document.getElementById("ns-canvas");			
 	ns.ctx = ns.canvas.getContext("2d");
-	ns.draw();
+	
+	ns.f = new ns.flake(100, -2, 5);	
+
+	ns.draw(ns.f);		
+	setInterval(ns.f.move,500);
 }
 
-ns.draw = function draw() {
+ns.flake = function (x, y, velocity){
+	this.x = x;
+	this.y = y;
+	this.velocity = velocity;
+	
+	this.move = function() {
+		ns.f.y = ns.f.y + ns.f.velocity;
+		console.log(ns.f);
+		console.log(this.y);
+	}
+}
+
+
+ns.draw = function draw(f) {
 	ns.ctx.clearRect(0, 0, ns.canvas.width, ns.canvas.height);
 	ns.ctx.beginPath();
 	ns.ctx.lineWidth="3";
 	ns.ctx.strokeStyle="green"; // Green path
 	ns.ctx.moveTo(10,10);
-	ns.ctx.lineTo(100,100+1);
-	ns.ctx.stroke();			
+	ns.ctx.lineTo(200,200);
+	ns.ctx.stroke();				
 }
