d77698c0d229d5d1337c9e8f324bbbf1062f7494
Lasse Loytynoja <loytynoja@gmail.com>
2013-03-12 20:06:50 +0200

    properties, createFlakes, removeFlakes added as well as array to hold the flakes

--- js/snowmachine.js
+++ js/snowmachine.js
@@ -2,14 +2,20 @@
 
 var ns = {};
 
+
+ns.flakes = [];
+
 ns.main = function main() {
 	ns.canvas = document.getElementById("ns-canvas");			
 	ns.ctx = ns.canvas.getContext("2d");
 	
-	ns.f = new ns.flake(100, -2, 5);	
-	//ns.draw(ns.f);		
-	setInterval(function(){ ns.draw(ns.f); },100);
-}
+	/* init properties */
+	ns.props = new ns.properties(10, 10, 10);
+	ns.createFlakes(ns.props.getFlakesPerRound());
+
+	/* start animation */
+	window.requestAnimationFrame(ns.draw(ns.f, ns.props));
+	}
 
 ns.flake = function (x, y, velocity){
 
@@ -25,16 +31,80 @@ ns.flake = function (x, y, velocity){
 	}
 }
 
+ns.properties = function (maxVelocity, flakesPerRound, minRate) {
 
-ns.draw = function draw(f) {
+	var counter = 0;
+	this.maxVelocity = maxVelocity;
+	this.flakesPerRound = flakesPerRound;
+	this.minRate = minRate;
+	
+	/* counter */
+	this.increaseCounter = function () {
+		counter++;
+		if (counter > 1000) {
+			counter = 0;
+		}
+	}
+	this.getCounterValue = function () {
+		return counter;
+	}
+	
+	/* flakes per addition */
+	this.setFlakesPerRound = function (flakeAmount) {
+		flakesPerRound = flakeAmount;
+	}
+	this.getFlakesPerRound = function () {
+		return flakesPerRound;
+	}
+	
+	/* minimum rate */
+	this.setMinRate = function (rate) {
+		minRate = rate;
+	}
+	this.getMinRate = function () {
+		return minRate;
+	}
+	
+	/* velocity */
+	this.setMaxVelocity = function (velocity) {
+		maxVelocity = velocity;
+	}
+	this.getMaxVelocity = function () {
+		return maxVelocity;
+	}
+}
 
-	f.move();
-	console.log(f.getY());
-	ns.ctx.clearRect(0, 0, ns.canvas.width, ns.canvas.height);
-	ns.ctx.beginPath();
-	ns.ctx.lineWidth="3";
-	ns.ctx.strokeStyle="white";
-	ns.ctx.moveTo(10,f.getY());
-	ns.ctx.lineTo(13,f.getY());
-	ns.ctx.stroke();				
+ns.removeFlakes = function (flakes) {
+
+	var newFlakeArr = []
+
+	for (var flake : flakes) {
+		if (flake.getY < ns.canvas.height) {
+			newFlakeArr.push(flake);
+		}
+	}	
+	flakes = newFlakeArr;
+}
+
+ns.createFlakes = function (amount) {
+
+	for (var i = 0; i < amount; i++) {
+		ns.flakes.push(new ns.flake(Math.random() * ns.canvas.width, -2, Math.random() * ns.maxVelocity));
+	}
+}
+
+/* luuppaa taulukko ja pirrä sisältö */
+ns.draw = function draw(f, props) {
+	setTimeout(function() {
+		f.move();
+		console.log(f.getY());
+		ns.ctx.clearRect(0, 0, ns.canvas.width, ns.canvas.height);
+		ns.ctx.beginPath();
+		ns.ctx.lineWidth="3";
+		ns.ctx.strokeStyle="white";
+		ns.ctx.moveTo(10,f.getY());
+		ns.ctx.lineTo(13,f.getY());
+		ns.ctx.stroke();
+		window.requestAnimationFrame(ns.draw(f));
+	}, 1000 / 25);
 }
